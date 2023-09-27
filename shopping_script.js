2862fbbe5c49b85a2ff4291c31a2b242633b5504
Monisha s <108780523+MonishaSugirthalingam@users.noreply.github.com>
2023-09-27 23:37:41 +0530

    Add files via upload
    Responsive Main Page with Filter pages

---
+++ shopping_script.js
@@ -0,0 +1,31 @@
+function openNav1() {
+    document.getElementById("women").style.width = "100%";
+  }
+  
+  function closeNav1() {
+    document.getElementById("women").style.width = "0%";
+  }
+
+  function openNav2() {
+    document.getElementById("men").style.width = "100%";
+  }
+  
+  function closeNav2() {
+    document.getElementById("men").style.width = "0%";
+  }
+
+  function openNav3() {
+    document.getElementById("kids").style.width = "100%";
+  }
+  
+  function closeNav3() {
+    document.getElementById("kids").style.width = "0%";
+  }   
+
+  function OpenSearchNav(){
+      document.getElementById("searchbar").style.height="20%";
+  }
+
+  function closeSearchNav(){
+      document.getElementById("searchbar").style.height="0%";
+  }
